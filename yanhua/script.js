efb5b2d84c71de8a14ec38480cc7b645607c73d3
hanabiicros <39493497+hanabiicros@users.noreply.github.com>
2022-12-30 17:38:49 +0800

    Update script.js

--- yanhua/script.js
+++ yanhua/script.js
@@ -131,7 +131,7 @@ const store = {
 	state: {
 		// will be unpaused in init()
 		paused: true,
-		soundEnabled: false,
+		soundEnabled: true,
 		menuOpen: false,
 		openHelpTopic: null,
 		fullscreen: isFullscreen(),
